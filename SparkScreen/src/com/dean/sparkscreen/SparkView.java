9eda2e66bf9bd40f29c69ca564b525ef9e5af158
Dean <396901990@qq.com>
2015-02-06 23:45:47 +0800

    remove unuse file

--- SparkScreen/src/com/dean/sparkscreen/SparkView.java
+++ SparkScreen/src/com/dean/sparkscreen/SparkView.java
@@ -10,20 +10,24 @@ public class SparkView extends SurfaceView implements SurfaceHolder.Callback {
 		super(context);
 	}
 
+	// Surface的大小发生改变时调用
 	@Override
-	public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
 		// TODO Auto-generated method stub
 
 	}
 
+	// Surface创建时激发，一般在这里调用画面的线程
 	@Override
-	public void surfaceCreated(SurfaceHolder arg0) {
+	public void surfaceCreated(SurfaceHolder holder) {
 		// TODO Auto-generated method stub
 
 	}
 
+	// 销毁时激发，一般在这里将画面的线程停止、释放。
 	@Override
-	public void surfaceDestroyed(SurfaceHolder arg0) {
+	public void surfaceDestroyed(SurfaceHolder argholder0) {
 		// TODO Auto-generated method stub
 
 	}
