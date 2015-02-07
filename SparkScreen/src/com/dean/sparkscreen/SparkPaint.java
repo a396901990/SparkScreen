ad27944f20e79870c7711a201d1a85afe48778cc
Dean <396901990@qq.com>
2015-02-08 01:52:07 +0800

    add sparkPaint

---
+++ SparkScreen/src/com/dean/sparkscreen/SparkPaint.java
@@ -0,0 +1,38 @@
+package com.dean.sparkscreen;
+
+import android.graphics.BlurMaskFilter;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+public class SparkPaint {
+
+	public Paint sparkPaint;
+
+	public SparkPaint() {
+
+		setSparkPaint();
+	}
+
+	private void setSparkPaint() {
+		sparkPaint = new Paint();
+		 // ´ò¿ª¿¹¾â³İ  
+		this.sparkPaint.setAntiAlias(true);
+		this.sparkPaint.setDither(true);
+	    /* 
+	     * ÉèÖÃ»­±ÊÑùÊ½ÎªÌî³ä
+	     * Paint.Style.STROKE£ºÃè±ß 
+	     * Paint.Style.FILL_AND_STROKE£ºÃè±ß²¢Ìî³ä 
+	     * Paint.Style.FILL£ºÌî³ä 
+	     */  
+		this.sparkPaint.setStyle(Paint.Style.FILL);
+		this.sparkPaint.setColor(Color.YELLOW);
+		this.sparkPaint.setMaskFilter(new BlurMaskFilter(15.0F,
+				BlurMaskFilter.Blur.NORMAL));
+	}
+
+	public void drawSpark(Canvas canvas, float x, float y) {
+		canvas.drawCircle(x, y, 30, sparkPaint);
+	}
+	
+}
--- SparkScreen/src/com/dean/sparkscreen/SparkView.java
+++ SparkScreen/src/com/dean/sparkscreen/SparkView.java
@@ -3,7 +3,7 @@ package com.dean.sparkscreen;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Paint;
+import android.graphics.PorterDuff;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -12,41 +12,14 @@ public class SparkView extends SurfaceView implements SurfaceHolder.Callback {
 
 	private SurfaceHolder mHolder;
 	private Canvas mCanvas;
-	private Paint mPaint;
+	private SparkPaint mSparkPaint;
 	
 	public SparkView(Context context) {
 		super(context);
-		initPaint();
 		mHolder = this.getHolder();
 		mHolder.addCallback(this);
+		mSparkPaint = new SparkPaint();
 	}
-	
-	/** 
-	 * 初始化画笔 
-	 */  
-	private void initPaint() {  
-	    // 实例化画笔并打开抗锯齿  
-	    mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);  
-	  
-	    /* 
-	     * 设置画笔样式为描边，圆环嘛……当然不能填充不然就么意思了 
-	     *  
-	     * 画笔样式分三种： 
-	     * 1.Paint.Style.STROKE：描边 
-	     * 2.Paint.Style.FILL_AND_STROKE：描边并填充 
-	     * 3.Paint.Style.FILL：填充 
-	     */  
-	    mPaint.setStyle(Paint.Style.STROKE);  
-	  
-	    // 设置画笔颜色为浅灰色  
-	    mPaint.setColor(Color.LTGRAY);  
-	  
-	    /* 
-	     * 设置描边的粗细，单位：像素px 
-	     * 注意：当setStrokeWidth(0)的时候描边宽度并不为0而是只占一个像素 
-	     */  
-	    mPaint.setStrokeWidth(10);  
-	} 
 
 	// Surface的大小发生改变时调用
 	@Override
@@ -70,6 +43,7 @@ public class SparkView extends SurfaceView implements SurfaceHolder.Callback {
 				drawSparkPonit(event.getX(), event.getY());
 				break;
 			case MotionEvent.ACTION_UP:
+				drawClear();
 				break;
 			case MotionEvent.ACTION_MOVE:
 				break;
@@ -84,8 +58,20 @@ public class SparkView extends SurfaceView implements SurfaceHolder.Callback {
 
 	private void drawSparkPonit(double x, double y) {
 		mCanvas = mHolder.lockCanvas();
-		mCanvas.drawCircle((int)x, (int)y, 50, mPaint);
-		mHolder.unlockCanvasAndPost(mCanvas);
+		if (mCanvas != null) {
+			mCanvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+			mSparkPaint.drawSpark(mCanvas, (float)x, (float)y);
+			mHolder.unlockCanvasAndPost(mCanvas);
+		}
+	}
+	
+	public void drawClear() {
+		mCanvas = mHolder.lockCanvas();
+		if (mCanvas != null) {
+			mCanvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+			// mCanvas.drawColor(Color.alpha(0));
+			mHolder.unlockCanvasAndPost(mCanvas);
+		}
 	}
 
 	// Surface创建时激发，一般在这里调用画面的线程
